975654b6479d626a8c7dcd769a2a1b14ab32bbcd
Vladimir <vgor555@gmail.com>
2018-12-24 09:09:17 +0300

    semicolon fixes

--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ gulp.task('browser-sync', function() {
     // open: false,
     // online: false, // Work Offline Without Internet Connection
     // tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
-  })
+  });
 });
 
 gulp.task('styles', function() {
@@ -52,7 +52,7 @@ gulp.task('styles', function() {
   .pipe(autoprefixer(['last 15 versions']))
   .pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
   .pipe(gulp.dest( paths.styles.dst ))
-  .pipe(browserSync.stream())
+  .pipe(browserSync.stream());
 });
 
 if (is_js_min){
@@ -62,7 +62,7 @@ if (is_js_min){
     .pipe(babel({presets: ['@babel/env']}))
     .pipe(uglify()) // Mifify js 
     .pipe(gulp.dest(paths.scripts.dst))
-    .pipe(browserSync.reload({ stream: true }))
+    .pipe(browserSync.reload({ stream: true }));
   });
 } else {
   gulp.task('scripts', function() {
@@ -70,7 +70,7 @@ if (is_js_min){
     .pipe(concat(js_file_name+'.js'))
     .pipe(babel({presets: ['@babel/env']}))
     .pipe(gulp.dest(paths.scripts.dst))
-    .pipe(browserSync.reload({ stream: true }))
+    .pipe(browserSync.reload({ stream: true }));
   });
 }
 
@@ -86,7 +86,7 @@ gulp.task('pug', function () {
   }).on('error', function(e) { console.log(e.message); })
   )
   .pipe(gulp.dest('./'+paths.dst))//   
-  .pipe(browserSync.reload({ stream: true }))
+  .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('rsync', function() {
@@ -101,14 +101,14 @@ gulp.task('rsync', function() {
     archive: true,
     silent: false,
     compress: true
-  }))
+  }));
 });
 
 if (gulpversion == 3) {
   gulp.task('watch', ['styles', 'scripts', 'pug', 'browser-sync'], function() {
     gulp.watch( paths.styles.src+'/**/*.{sass,scss}', ['styles'] );
     gulp.watch( paths.scripts.src+'/**/*.js', ['scripts'] );
-    gulp.watch( paths.src+'/**/!(_)*.pug', ['pug'] )
+    gulp.watch( paths.src+'/**/!(_)*.pug', ['pug'] );
   });
   gulp.task('default', ['watch']);
 }
@@ -117,7 +117,7 @@ if (gulpversion == 4) {
   gulp.task('watch', function() {
     gulp.watch( paths.styles.src+'/**/*.{sass,scss}', gulp.parallel('styles') );
     gulp.watch( paths.scripts.src+'/**/*.js', gulp.parallel('scripts') );
-    gulp.watch( paths.src+'/**/!(_)*.pug', gulp.series('pug') )//,'code'
+    gulp.watch( paths.src+'/**/!(_)*.pug', gulp.series('pug') );//,'code'
   });
   gulp.task('default', gulp.parallel('watch', 'pug', 'styles', 'scripts', 'browser-sync'));
 }
